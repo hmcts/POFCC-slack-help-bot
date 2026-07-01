e7040183b870afe8c5feedacc44293a4d9857c43
padmapriyanalam <padmapriya.nalam@gmail.com>
2026-07-01 15:01:15 +0100

    POFCC-257 Add EPIC Name

--- src/service/persistence.js
+++ src/service/persistence.js
@@ -184,17 +184,15 @@ async function createHelpRequest({
 
     const project = await jira.getProject(jiraProject);
 
-    const epicName = await jira.getEpicName(jiraEpicName);
-
     // https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issues/#api-rest-api-2-issue-post
     // note: fields don't match 100%, our Jira version is a bit old (still a supported LTS though)
 
     let result
     try {
-        result = await createHelpRequestInJira(summary, project, user, labels, epicName);
+        result = await createHelpRequestInJira(summary, project, user, labels, jiraEpicName);
     } catch(err) {
         // in case the user doesn't exist in Jira use the system user
-        result = await createHelpRequestInJira(summary, project, systemUser, labels, epicName);
+        result = await createHelpRequestInJira(summary, project, systemUser, labels, jiraEpicName);
 
         if (!result.key) {
             console.log("Error creating help request in jira", JSON.stringify(result));
